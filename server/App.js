a248151699ae8fa2e3300fb9638767482298c73a
theobondarec <47282766+theobondarec@users.noreply.github.com>
2020-10-14 17:39:46 +0200

    Middleware step

--- server/App.js
+++ server/App.js
@@ -1,8 +1,22 @@
 const express = require('express')
 const app = express()
 
+const customMiddleware = (res, req, next)=>{
+    console.log("Middleware executed")
+    next()
+}
+
+//Middleware executed every time
+// app.use(customMiddelware)
+
 app.get('/', (req, res)=>{
+    console.log("HomePage")
     res.send("HelloWorld")
+})
+
+app.get('/about', /*customMiddleware,*/ (req, res)=>{   //Middleware executed only for about page
+    console.log("aboutPage")
+    res.send("about this website")
 })
 
 const PORT = 5000
